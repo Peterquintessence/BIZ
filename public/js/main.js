38d33103b0aa30981868e97aaf0ec3003116b367
Peter Quinn <peterquinn@vanillawebsites.co.uk>
2024-08-07 15:21:51 +0100

    Fix template errors and specify Hugo version for Netlify

--- public/js/main.js
+++ public/js/main.js
@@ -1,28 +1,2 @@
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
-
-document.addEventListener('DOMContentLoaded', function () {
-  const themeToggle = document.getElementById('themeToggle');
-  const htmlElement = document.documentElement; // Get the <html> element
-
-  // Check stored theme preference
-  const currentTheme = localStorage.getItem('theme') || 'light';
-  if (currentTheme === 'dark') {
-    htmlElement.setAttribute('data-bs-theme', 'dark');
-    themeToggle.checked = true;
-  } else {
-    htmlElement.setAttribute('data-bs-theme', 'light');
-  }
-
-  // Toggle theme on checkbox change
-  themeToggle.addEventListener('change', function () {
-    if (this.checked) {
-      htmlElement.setAttribute('data-bs-theme', 'dark');
-      localStorage.setItem('theme', 'dark');
-    } else {
-      htmlElement.setAttribute('data-bs-theme', 'light');
-      localStorage.setItem('theme', 'light');
-    }
-  });
-});
-
